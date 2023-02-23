6dab9b41ca1893fe34c92689ccde4ba636e575fb
Druiz45 <druiz45@itfip.edu.co>
2023-02-22 20:03:54 -0500

    Cambios a la clase usuario

--- app/usuario/Usuario.php
+++ app/usuario/Usuario.php
@@ -4,19 +4,31 @@ namespace app\usuario;
 
 class Usuario{
 
-    private $ciudad;
+    // protected $ciudad;
     protected $nombre;
-    private $apellido;
-    private $tipoDocumento;
-    private $documento;
-    private $sexo;
-    private $email;
-    private $telefono;
-    private $fechaNacimiento;
-    private $usuario;
-    private $deporte;
+    protected $apellido;
+    protected $tipoDocumento;
+    protected $documento;
+    protected $sexo;
+    protected $email;
+    protected $telefono;
+    protected $fechaNacimiento;
+    protected $usuario;
+    protected $deporte;
 
-    public function __construct(){
+    public function __construct($nombre, $apellido, $tipoDocumento, $documento, 
+    $sexo, $email, $telefono, $fechaNacimiento, $usuario, $deporte){
+
+        $this->nombre = $nombre;
+        $this->apellido = $apellido;
+        $this->tipoDocumento = $tipoDocumento;
+        $this->documento = $documento;
+        $this->sexo = $sexo;
+        $this->email = $email;
+        $this->telefono = $telefono;
+        $this->fechaNacimiento = $fechaNacimiento;
+        $this->usuario = $usuario;
+        $this->deporte = $deporte;
         
     }
 
