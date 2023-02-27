d25dcf1327b71f92f1a7e567501dfa6122c1622b
JuanVaquiro <jvaquiro23@itfip.edu.co>
2023-02-27 14:55:02 -0500

    styles: new-btn-event and new div container

--- views/viewHome.php
+++ views/viewHome.php
@@ -1,27 +1,33 @@
 <section class="flex flex-col  justify-center items-center h-screen bg-gray-900">
-<div class="dropdown relative inline-block ml-80">
-  <a id="drop" href="#" class="text-gray-300">
-    <svg class="w-10" clip-rule="evenodd" fill="#fff" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m13 16.75c0-.414-.336-.75-.75-.75h-9.5c-.414 0-.75.336-.75.75s.336.75.75.75h9.5c.414 0 .75-.336.75-.75zm2.195-5.992 2.746 2.999c.142.154.342.243.552.243s.41-.088.553-.242l2.757-2.999c.132-.144.197-.326.197-.507 0-.684-.841-1.008-1.303-.508l-2.202 2.397-2.194-2.396c-.46-.503-1.303-.175-1.303.507 0 .18.065.362.197.506zm-2.195.992c0-.414-.336-.75-.75-.75h-9.5c-.414 0-.75.336-.75.75s.336.75.75.75h9.5c.414 0 .75-.336.75-.75zm0-5c0-.414-.336-.75-.75-.75h-9.5c-.414 0-.75.336-.75.75s.336.75.75.75h9.5c.414 0 .75-.336.75-.75z" fill-rule="nonzero"/>
-    </svg>  
-  </a>
-  <div class="menu bg-gray-800">
-    <ul class="space-y-3">
-      <li>
-        <a href="#" class="flex gap-2 m-3 px-2 py-3 text-white hover-custom">
-    <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"></path></svg>
-      Perfil
+  <div class="flex items-center p-1 gap-32 sm:gap-96">
+    <a class="cursor-pointer source-sans-pro border focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 border-purple-400 text-purple-400 hover:text-white hover:bg-purple-500 focus:ring-purple-900">
+      Ver mis estadisticas
     </a>
-  </li>
-  <li>
-  <a id="logOut" href="#" class="flex gap-2 m-3 px-2 py-3 text-white hover-custom">
-    <svg class="w-6 h-6 text-red-600" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"></path></svg>
-      Cerrar Session
+    <div class="dropdown relative inline-block">
+      <a id="drop" href="#" class="text-gray-300">
+        <svg class="w-10" clip-rule="evenodd" fill="#fff" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m13 16.75c0-.414-.336-.75-.75-.75h-9.5c-.414 0-.75.336-.75.75s.336.75.75.75h9.5c.414 0 .75-.336.75-.75zm2.195-5.992 2.746 2.999c.142.154.342.243.552.243s.41-.088.553-.242l2.757-2.999c.132-.144.197-.326.197-.507 0-.684-.841-1.008-1.303-.508l-2.202 2.397-2.194-2.396c-.46-.503-1.303-.175-1.303.507 0 .18.065.362.197.506zm-2.195.992c0-.414-.336-.75-.75-.75h-9.5c-.414 0-.75.336-.75.75s.336.75.75.75h9.5c.414 0 .75-.336.75-.75zm0-5c0-.414-.336-.75-.75-.75h-9.5c-.414 0-.75.336-.75.75s.336.75.75.75h9.5c.414 0 .75-.336.75-.75z" fill-rule="nonzero"/>
+      </svg>  
     </a>
-  </li>
-    </ul>
+    <div class="menu bg-gray-800">
+        <ul class="space-y-3">
+          <li>
+            <a href="#" class="flex gap-2 m-3 px-2 py-3 text-white hover-custom">
+              <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"></path>
+            </svg>
+              Perfil
+            </a>
+          </li>
+          <li>
+            <a id="logOut" href="#" class="flex gap-2 m-3 px-2 py-3 text-white hover-custom">
+              <svg class="w-6 h-6 text-red-600" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"></path>
+              </svg>
+                Cerrar Session
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
-  
-</div>
     </div>
     <div class="flex flex-col items-center">
         <img class="w-1/4" src="./../../dist/img/analytics.png" alt="logo-DataFight">
@@ -29,17 +35,17 @@
             Data Fight
         </h2>
         <div class="grid grid-cols-2 gap-3">
-            <a class="source-sans-pro border focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 border-purple-400 text-purple-400 hover:text-white hover:bg-purple-500 focus:ring-purple-900">
-              Ver mis estadisticas
-            </a>
-            <a class="source-sans-pro border focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 border-sky-500 text-sky-500 hover:text-white hover:bg-sky-600 focus:ring-sky-800">
+            <a class="cursor-pointer source-sans-pro border focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 border-sky-500 text-sky-500 hover:text-white hover:bg-sky-600 focus:ring-sky-800">
               Configurar combate
             </a>
-            <a class="source-sans-pro border focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 border-sky-500 text-sky-500 hover:text-white hover:bg-sky-600 focus:ring-sky-800">
-              Creear evento
+            <a class="cursor-pointer source-sans-pro border focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 border-sky-500 text-sky-500 hover:text-white hover:bg-sky-600 focus:ring-sky-800">
+              Creear Deportista
             </a>
-            <a class="source-sans-pro border focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 border-sky-500 text-sky-500 hover:text-white hover:bg-sky-600 focus:ring-sky-800">
-               Entrar como control #2
+            <a class="cursor-pointer source-sans-pro border focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 border-sky-500 text-sky-500 hover:text-white hover:bg-sky-600 focus:ring-sky-800">
+              Creear Delegacion
+            </a>
+            <a class="cursor-pointer source-sans-pro border focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 border-sky-500 text-sky-500 hover:text-white hover:bg-sky-600 focus:ring-sky-800">
+              Creear evento
             </a>
         </div>
     </div>    
