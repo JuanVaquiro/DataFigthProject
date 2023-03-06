ec949d0cc112b3629fe46a9bf678e48e0df58305
JuanVaquiro <jvaquiro23@itfip.edu.co>
2023-03-06 13:30:28 -0500

    styles: control

--- views/viewControl.php
+++ views/viewControl.php
@@ -1,32 +1,32 @@
-<section>
+<section class="bg-slate-900 source-sans-pro">
   <div class="flex justify-around items-center h-80">
     <!-- acierto del golpe -->
     <div class="btns-group1 grid grid-cols-2 gap-3">
-      <button type="button" id="casco" class="h-20 w-28 focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2">
+      <button type="button" id="casco" class="h-20 w-28 focus:outline-none border border-gray-500 text-white bg-yellow-700 hover:bg-yellow-500 hover:border-gray-800 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2">
         Casco
       </button>
-      <button type="button" id="delante" class="h-20 w-28 focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2">
-        Delante
+      <button type="button" id="delante" class="h-20 w-28 focus:outline-none border border-gray-500 text-white bg-yellow-900 hover:bg-yellow-500 hover:border-gray-800 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2">
+        Delantera
       </button>
       <div>
-        <button id="confirmar_punto" class="z-50 fixed border-2  font-medium rounded-bl-2xl rounded-b-2xl  text-sm px-1 py-2 text-center mr-2 mb-1 border-green-600 text-white hover:bg-green-500">
+        <button id="confirmar_punto" class="z-50 fixed border-2 border-green-700 font-medium rounded-bl-2xl rounded-b-2xl  text-sm px-1 py-2 text-center mr-2 mb-1  text-white hover:bg-green-500">
           +
         </button>
-        <button type="button" id="pechera" class="h-20 w-28 focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2">
+        <button type="button" id="pechera" class="h-20 w-28 focus:outline-none border border-gray-500 text-white bg-yellow-700 hover:bg-yellow-500 hover:border-gray-800 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2">
           Pechera
         </button>
       </div>
-      <button type="button" id="trasera" class="h-20 w-28 focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2">
+      <button type="button" id="trasera" class="h-20 w-28 focus:outline-none border border-gray-500 text-white bg-yellow-900 hover:bg-yellow-500 hover:border-gray-800 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2">
         Trasera
       </button>
-      <button type="button" id="guardar_movimiento" class="div1 focus:outline-none text-white bg-green-400 hover:bg-green-500 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5">
+      <button type="button" id="guardar_movimiento" class="div1 focus:outline-none border border-gray-500 text-white bg-green-800 hover:bg-green-700 focus:ring-4 focus:ring-green-600 font-medium rounded-lg text-sm px-5 py-2.5">
         GUARDAR
       </button>
     </div>
     <!-- Menu -->
-    <article class="flex flex-col justify-center gap-2 bg-slate-400 p-2 h-80">
+    <article class="flex flex-col justify-center gap-2 text-white bg-slate-800 shadow-lg border border-slate-500/30 rounded-tl p-2 h-80 ring-1 ring-white/10 ring-inset">
       <button type="button" id="finalizar_combate" class="p-4">Finalizar Combate</button>
-      <button type="button" id="iniciar_ronda" class="p-4">Iniciar Ronda</button>
+      <button type="button" id="iniciar_ronda" class="p-4">INICIAR</button>
       <button type="button" id="pausar" class="p-4">Pausar</button>
       <button type="button" id="regresar" class="p-4">Regresar</button>
     <!-- Modal toggle --> 
@@ -36,59 +36,59 @@
     </article>
     <!-- Gulpes -->
     <div class="grid grid-cols-2 gap-3">
-      <button type="button" id="puño_izquierdo" class="h-20 w-28 border focus:outline-none focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 bg-gray-800 text-white border-gray-600 hover:bg-gray-700 hover:border-gray-600 focus:ring-gray-700">
-        Puño izquierdo
-      </button>
-      <button type="button" id="puño_derecho" class="h-20 w-28 border focus:outline-none focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 bg-gray-800 text-white border-gray-600 hover:bg-gray-700 hover:border-gray-600 focus:ring-gray-700">
+      <button type="button" id="puño_derecho" class="h-20 w-28 border focus:outline-none focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 bg-red-900 text-white border-gray-500 hover:bg-red-500 hover:border-gray-600 focus:ring-gray-700">
         Puño derecho
       </button>
-      <button type="button" id="patada_izquierda" class="h-20 w-28 border focus:outline-none focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 bg-gray-800 text-white border-gray-600 hover:bg-gray-700 hover:border-gray-600 focus:ring-gray-700"
-      >Patada izquierda
-    </button>
-      <button type="button" id="patada_derecha" class="h-20 w-28 border focus:outline-none focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 bg-gray-800 text-white border-gray-600 hover:bg-gray-700 hover:border-gray-600 focus:ring-gray-700">
+      <button type="button" id="patada_derecha" class="h-20 w-28 border focus:outline-none focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 bg-red-700 text-white border-gray-500 hover:bg-red-500 hover:border-gray-600 focus:ring-gray-700">
         Patada derecha
       </button>
-      <button type="button" id="patada_con_giro" class="border focus:outline-none focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 bg-gray-800 text-white border-gray-600 hover:bg-gray-700 hover:border-gray-600 focus:ring-gray-700">
+      <button type="button" id="puño_izquierdo" class="h-20 w-28 border focus:outline-none focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 bg-red-900 text-white border-gray-500 hover:bg-red-500 hover:border-gray-600 focus:ring-gray-700">
+        Puño izquierdo
+      </button>
+      <button type="button" id="patada_izquierda" class="h-20 w-28 border focus:outline-none focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 bg-red-700 text-white border-gray-500 hover:bg-red-500 hover:border-gray-600 focus:ring-gray-700">
+        Patada izquierda
+      </button>
+      <button type="button" id="patada_con_giro" class="border focus:outline-none focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2
+       bg-red-500 text-white border-gray-500 hover:bg-red-500 hover:border-gray-600 focus:ring-gray-700">
         Patada con giro
       </button>
     </div>
   </div>
   <!-- informacion -->
-  <article class="flex justify-between bg-sky-300">
-    <div class="flex flex-col  mx-3 p-2 w-5/12 md:w-1/2">
-      <span class="" id="deportista">Andrea Camila Ramirez</span>
-      <span class="" id="delegacion">Liga Boyaca</span>
-    </div>
+  <article class="flex justify-between  bg-slate-400">
+    <dl class="flex flex-col  mx-3 p-2 w-5/12 md:w-1/2">
+      <dt class="" id="deportista">nombre No econtrado</dt>
+      <dd class="" id="delegacion">delegacion No econtrado</dd>
+    </dl>
     <div class="flex">
-    <div class="flex flex-col items-center mx-3 p-2">
-        <span class="">Fase</span>
-        <span class="" id="fase">semifinal</span>
+    <dl class="flex flex-col items-center mx-3 p-2">
+      <dt class="">Fase</dt>
+      <dd class="" id="fase">NAN</dd>
+    </dl>
+    <dl class="flex flex-col items-center mx-3 p-2">
+      <dt class="">Round</dt>
+      <dd id="round" class="py-1 px-4 rounded-lg shadow-md shadow-slate-800 text-white bg-slate-600">0</dd>
+    </dl>
+    <dl class="flex flex-col items-center mx-3 p-2">
+      <dt class="">Faltas</dt>
+      <dd id="contador_faltas" class="py-1 px-4 rounded-lg shadow-md shadow-slate-800 text-white bg-slate-600">0</dd>
+    </dl>
+    <dl class="flex flex-col items-center mx-3 p-2">
+      <div class="flex gap-1 items-center">
+        <dt>Tiempo</dt>
+        <button id="cambiar_tiempo" type="button">
+          <svg height="21" viewBox="0 0 21 21" width="21" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" transform="matrix(-1 0 0 1 19 2)"><circle cx="8.5" cy="8.5" r="8"/><path d="m8.5 5.5v4h-3.5"/></g></svg>
+        </button>
       </div>
-      <div class="flex flex-col items-center mx-3 p-2">
-        <span class="">Round</span>
-        <span id="round" class="">0</span>
-      </div>
-      <div class="flex flex-col items-center mx-3 p-2">
-        <span class="">Faltas</span>
-        <span id="contador_faltas" class="">0</span>
-      </div>
-      <div class="flex flex-col items-center mx-3 p-2">
-        <div class="flex gap-1 items-center">
-          <span>Tiempo</span>
-          <button id="cambiar_tiempo" type="button">
-            <svg height="21" viewBox="0 0 21 21" width="21" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" transform="matrix(-1 0 0 1 19 2)"><circle cx="8.5" cy="8.5" r="8"/><path d="m8.5 5.5v4h-3.5"/></g></svg>
-          </button>
-        </div>
-        <span id="tiempo" class="">00:00</span>
-      </div>
-    </div>
+        <dd id="tiempo" class="py-1 px-4 rounded-lg shadow-md shadow-slate-800  text-white bg-slate-600">00:00</dd>
+    </dl>
   </article>
 </section>
 <!-- modal tiempo terminado -->
   <div id="modal" class="modal">
     <div class="modal-content">
       <div class="flex items-start justify-between w-full p-4 border-b rounded-t border-gray-600">
-      <h3 class="source-sans-pro text-xl font-semibold text-white">
+      <h3 class="text-xl font-semibold text-white">
         Final Round
       </h3>
       <button id="close_button_modal" type="button" class="text-gray-400 bg-transparent rounded-lg text-sm p-1.5 ml-auto inline-flex items-center hover:bg-gray-600 hover:text-white">
@@ -101,7 +101,7 @@
       <!-- modal pausar -->
   <div id="modal_contiainer" class="modal">
     <div class="modal-content">
-      <p id="modal_meesage" class="source-sans-pro text-xl font-semibold text-white"></p>
+      <p id="modal_meesage" class="text-xl font-semibold text-white"></p>
       <button id="modal_close_message" class="text-white focus:ring-4 focus:outline-none  font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-blue-600 hover:bg-blue-700 focus:ring-blue-800" >Reanudar</button>
     </div>
   </div>
@@ -111,7 +111,7 @@
   <div class="modal-content">
     <div class="flex items-start justify-between w-full p-4 border-b rounded-t border-gray-600">
       <!-- Modal header tiempo -->
-      <h3 class="source-sans-pro text-xl font-semibold text-white">
+      <h3 class="text-xl font-semibold text-white">
         Cambiar tiempo
       </h3>
       <button id="cerrar_modal" type="button" class="text-gray-400 bg-transparent rounded-lg text-sm p-1.5 ml-auto inline-flex items-center hover:bg-gray-600 hover:text-white">
@@ -138,7 +138,7 @@
               <div class="relative rounded-lg shadow bg-gray-700">
                   <!-- Modal header -->
                   <div class="flex items-start justify-between p-4 border-b rounded-t border-gray-600">
-                      <h3 class="source-sans-pro text-xl font-semibold text-white">
+                      <h3 class="text-xl font-semibold text-white">
                           Seleccione una falta
                       </h3>
                       <button type="button" class="text-gray-400 bg-transparent rounded-lg text-sm p-1.5 ml-auto inline-flex items-center hover:bg-gray-600 hover:text-white" data-modal-hide="defaultModal">
