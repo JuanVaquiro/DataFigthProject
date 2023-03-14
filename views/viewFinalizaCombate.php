eeec9bed2d6f1eac0787519b898d54d547de27a7
JuanVaquiro <jvaquiro23@itfip.edu.co>
2023-03-14 12:46:31 -0500

    styles: file finalizarCombate.php

--- views/viewFinalizaCombate.php
+++ views/viewFinalizaCombate.php
@@ -1,31 +1,125 @@
 <?php
 
 ?>
+<section class="py-10 bg-gray-900 sm:py-16 lg:py-24">
+  <div class="flex flex-col gap-2 p-3 items-center justify-center">
+    <h2
+      class="magra-bold focus:outline-none xl:text-5xl md:text-3xl text-xl text-center text-white font-extrabold mb-1 pt-4">
+      Informacion del combate
+    </h2>
+    <h3
+      class="source-sans-pro focus:outline-none xl:text-3xl md:text-xl text-lg text-center text-gray-200 font-extrabold mb-5 pt-4">
+      Combate finalizdo
+    </h3>
+  </div>
+  <div class="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
+    <div class="grid grid-cols-1 gap-10 mx-auto text-center sm:grid-cols-2 md:grid-cols-2 lg:gap-y-12">
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Combate Finalizado</title>
-</head>
-<body>
-  <section>
-    <div>
-      <h2>Informaacion del combate</h2>
-      <p>nombre julio coreal</p>
-      <p>selecion; tolirma</p>
-      <p>ronda: data</p>
+      <div>
+        <div class="relative flex items-center justify-center mx-auto">
+          <svg class="text-sky-100" width="62" height="64" viewBox="0 0 62 64" fill="currentColor"
+            xmlns="http://www.w3.org/2000/svg">
+            <path
+              d="M62 13.001C62 33.4355 53.9345 64.001 33.5 64.001C13.0655 64.001 0 50.435 0 30.0005C0 9.56596 2.56546 4.00021 23 4.00021C43.4345 4.00021 62 -7.43358 62 13.001Z" />
+          </svg>
+          <svg class="absolute text-sky-600 w-9 h-9" xmlns="http://www.w3.org/2000/svg"
+            class="icon icon-tabler icon-tabler-user" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5"
+            stroke="#00abfb" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <circle cx="12" cy="7" r="4" />
+            <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
+          </svg>
+        </div>
+        <h3 class="source-sans-pro mt-8 text-lg font-semibold text-white">Deportista #1</h3>
+        <dl class="mt-4 flex gap-2 items-center justify-center">
+          <dt class="kanit-ligth text-base text-gray-300">Deportista:</dt>
+          <dd class="kanit-ligth text-base font-medium text-white">Andres Guzman</dd>
+        </dl>
+        <dl class="flex gap-2 items-center justify-center">
+          <dt class="kanit-ligth text-base text-gray-300">Delegacion:</dt>
+          <dd class="kanit-ligth text-base font-medium text-white">Seleccion Tolima</dd>
+        </dl>
+        <dl class="flex gap-2 items-center justify-center">
+          <dt class="kanit-ligth text-base text-gray-300">Ronda:</dt>
+          <dd class="kanit-ligth text-base font-medium text-white">Semifinales</dd>
+        </dl>
+      </div>
+
+      <div>
+        <div class="relative flex items-center justify-center mx-auto">
+          <svg class="text-orange-100" width="62" height="64" viewBox="0 0 62 64" fill="currentColor"
+            xmlns="http://www.w3.org/2000/svg">
+            <path
+              d="M62 13.001C62 33.4355 53.9345 64.001 33.5 64.001C13.0655 64.001 0 50.435 0 30.0005C0 9.56596 2.56546 4.00021 23 4.00021C43.4345 4.00021 62 -7.43358 62 13.001Z" />
+          </svg>
+          <svg class="absolute text-orange-600 w-9 h-9" xmlns="http://www.w3.org/2000/svg"
+            class="icon icon-tabler icon-tabler-chart-arcs" width="44" height="44" viewBox="0 0 24 24"
+            stroke-width="1.5" stroke="#ff9300" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <circle cx="12" cy="12" r="1" />
+            <path d="M16.924 11.132a5 5 0 1 0 -4.056 5.792" />
+            <path d="M3 12a9 9 0 1 0 9 -9" />
+          </svg>
+
+        </div>
+        <h3 class="source-sans-pro mt-8 text-lg font-semibold text-white">Informacion</h3>
+        <dl class="mt-4 m-1 flex gap-2 items-center justify-center">
+          <dt class="kanit-ligth text-base text-gray-300">Round Totalales:</dt>
+          <dd
+            class="kanit-ligth text-base font-medium text-white py-1 px-4 rounded-lg shadow-md shadow-slate-800 bg-slate-600">
+            3</dd>
+        </dl>
+        <dl class="flex m-1 gap-2 items-center justify-center">
+          <dt class="kanit-ligth text-base text-gray-300">Duracion del Round:</dt>
+          <dd
+            class="kanit-ligth text-base font-medium text-white py-1 px-4 rounded-lg shadow-md shadow-slate-800 bg-slate-600">
+            3 min</dd>
+        </dl>
+        <dl class="flex m-1 gap-2 items-center justify-center">
+          <dt class="kanit-ligth text-base text-gray-300">Round Ganados:</dt>
+          <dd
+            class="kanit-ligth text-base font-medium text-white py-1 px-4 rounded-lg shadow-md shadow-slate-800 bg-slate-600">
+            2</dd>
+        </dl>
+        <dl class="flex m-1 gap-2 items-center justify-center">
+          <dt class="kanit-ligth text-base text-gray-300">Faltas Totales:</dt>
+          <dd
+            class="kanit-ligth text-base font-medium text-white py-1 px-4 rounded-lg shadow-md shadow-slate-800 bg-slate-600">
+            12</dd>
+        </dl>
+      </div>
+
+      <div>
+        <div class="relative flex items-center justify-center mx-auto">
+          <svg class="text-green-100" width="62" height="64" viewBox="0 0 62 64" fill="currentColor"
+            xmlns="http://www.w3.org/2000/svg">
+            <path
+              d="M62 13.001C62 33.4355 53.9345 64.001 33.5 64.001C13.0655 64.001 0 50.435 0 30.0005C0 9.56596 2.56546 4.00021 23 4.00021C43.4345 4.00021 62 -7.43358 62 13.001Z" />
+          </svg>
+          <svg class="absolute text-green-600 w-9 h-9" xmlns="http://www.w3.org/2000/svg"
+            class="icon icon-tabler icon-tabler-message" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5"
+            stroke="#00b341" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M4 21v-13a3 3 0 0 1 3 -3h10a3 3 0 0 1 3 3v6a3 3 0 0 1 -3 3h-9l-4 4" />
+            <line x1="8" y1="9" x2="16" y2="9" />
+            <line x1="8" y1="13" x2="14" y2="13" />
+          </svg>
+        </div>
+        <div class="flex flex-col mt-8 p-4 lg:px-28 lg:py-2">
+          <label class="source-sans-pro text-base font-semibold leading-none text-gray-800 dark:text-white">
+            Escribe algun comentario del combate
+          </label>
+          <textarea tabindex="0" aria-label="leave a message" role="textbox" type="name"
+            class="h-36 text-base leading-none text-gray-900 p-3 focus:oultine-none focus:border-sky-600 mt-4 bg-gray-100 border rounded border-gray-200 placeholder-gray-100 resize-none"></textarea>
+        </div>
+      </div>
+
+      <div class="flex items-center justify-center">
+        <a href="#"
+          class="w-2/4 cursor-pointer source-sans-pro border focus:ring-4 focus:outline-none bg-sky-600 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 border-sky-500 text-sky-100 hover:text-white hover:bg-transparent focus:ring-sky-800">
+            GUARDAR 
+        </a>
+      </div>
+
     </div>
-    <div>
-      <p>faltas:</p>
-      <p>numero de rondas:</p>
-      <p>round ganado:</p>
-    </div>
-    <label for="">comentario</label>
-    <input type="text" value="">
-    <button>Guardar</button>
-  </section>
-</body>
-</html>
+</section>
