4b046337a429a92fd2bb95ba9bc616ba06438e20
JuanVaquiro <jvaquiro23@itfip.edu.co>
2023-04-12 16:34:11 -0500

    fix: mediquery 1520px

--- views/viewCrearArbitro.php
+++ views/viewCrearArbitro.php
@@ -4,7 +4,7 @@ require_once('./../../views/Components/Navbar.php');
 require_once('./../../views/Components/Sidebar.php');
 ?>
 
-<section class="bg-white p-8 sm:ml-64 dark:bg-gray-900">
+<section class="bg-white p-8 sm:ml-64 2xl:h-screen dark:bg-gray-900">
     <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-full md:p-10">
         <div
             class="my-5 w-full rounded-lg shadow border 2xl:max-w-screen-2xl lg:max-w-screen-lg md:mt-0 sm:max-w-2xl xl:p-0 bg-gray-50 dark:bg-gray-800 border-gray-700">
